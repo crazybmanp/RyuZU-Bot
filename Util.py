1eae9782192520ea7310476a3cf62e3b0bace4fa
Crazybmanp <crazybmanp@gmail.com>
2017-04-08 06:45:54 -0500

    Limited some of the util functions a bit, they could do some crazy things.

--- Util.py
+++ Util.py
@@ -16,12 +16,19 @@ class Util():
             await self.bot.say('Format has to be in NdN!')
             return
 
+        if rolls > 100 or limit > 1000000:
+            await self.bot.say("You cannot roll more than 100 dice, or dice bigger than 1 Million.")
+            return
+
         result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
         await self.bot.say(result)
 
     @commands.command(description='For when you wanna settle the score some other way')
     async def choose(self, *choices: str):
         """Chooses between multiple choices."""
+        if len(choices < 1):
+            await self.bot.say("You need to specify one or more choices.")
+            return
         await self.bot.say(random.choice(choices))
 
 
