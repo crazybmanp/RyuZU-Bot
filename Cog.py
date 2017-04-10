c907a20759f8e0dc385cd4cff7d4f0f2485c8c53
Crazybmanp <crazybmanp@gmail.com>
2017-04-10 00:38:18 -0500

    unless a huge error happens, this is it.

--- Cog.py
+++ Cog.py
@@ -1,3 +1,9 @@
+from os import makedirs
+from os.path import join, exists
+
+from tinydb import TinyDB
+
+
 class Cog:
     def __init__(self, bot, settings=None):
         self.bot = bot
@@ -12,3 +18,14 @@ class Cog:
                 self.bot.config[self.__class__.__name__] = self.settings
 
             self.bot.update_config()
+
+    def get_cog_db(self, databasename: str):
+        """
+        Mounts a db for a cog (handling naming)
+        :param databasename: the name of the database to fetch for your cog
+        :return: TinyDB database
+        """
+        directory = join("Databases", self.__class__.__name__)
+        if not exists(directory):
+            makedirs(directory)
+        return TinyDB(join("Databases", self.__class__.__name__, "{}.json".format(databasename)))
